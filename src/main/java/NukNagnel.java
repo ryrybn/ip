7636ba45067ddc1c7daaeab1775082b3eed420bb
Neo Ryan <ryanneoruiyan@gmail.com>
2026-01-23 15:25:01 +0800

    Week 2 Level-1 completed
    Signed-off-by: Neo Ryan <ryanneoruiyan@gmail.com>

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class NukNagnel {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
@@ -5,7 +7,14 @@ public class NukNagnel {
                 " What can I do for you?\n" +
                 "____________________________________________________________";
         System.out.println(intro);
-
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        do {
+            userInput = scanner.next();
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        } while (!userInput.equals("bye"));
         String exit = " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(exit);
