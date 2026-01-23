ccf17895dcb65db99b01480c3cafdf21d04cb7f8
Neo Ryan <ryanneoruiyan@gmail.com>
2026-01-23 15:09:01 +0800

    Week 2 Level-0 completed
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>
    
    # Conflicts:
    #	src/main/java/NukNagnel.java

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class NukNagnel {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
@@ -7,14 +5,7 @@ public class NukNagnel {
                 " What can I do for you?\n" +
                 "____________________________________________________________";
         System.out.println(intro);
-        Scanner scanner = new Scanner(System.in);
-        String userInput = "";
-        do {
-            userInput = scanner.next();
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
-        } while (!userInput.equals("bye"));
+
         String exit = " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(exit);
