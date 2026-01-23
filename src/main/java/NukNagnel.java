0afcdae0de5420bb6379409c8ecb5b21fa7ff9f1
Neo Ryan <ryanneoruiyan@gmail.com>
2026-01-23 15:09:01 +0800

    Week 2 Level-0 completed

---
+++ src/main/java/NukNagnel.java
@@ -0,0 +1,13 @@
+public class NukNagnel {
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________\n" +
+                " Hello! I'm NukNagnel\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________";
+        System.out.println(intro);
+
+        String exit = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(exit);
+    }
+}
