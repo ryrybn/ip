f4ec3baeaa5889c29f6bb2a775f712f9bbfeacf8
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-01-30 04:58:56 +0800

    NukNagnel.java: Added support for deleting tasks from the list, as well as relevant testcases
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
 public class NukNagnel {
-    private Task[] items = new Task[100];
-    private int itemsPtr = 0;
+    private ArrayList<Task> items = new ArrayList<>();
 
     // Sample data to test functionality
 //    private void samplePopulate() {
-//        items[0] = new ToDo("read book");
-//        items[0].markAsDone();
-//        items[1] = new Deadline("return book", "June 6th");
-//        items[2] = new Event("project meeting", "Aug 6th 2pm", "4pm");
-//        items[3] = new ToDo("joins sports club");
-//        items[3].markAsDone();
-//        itemsPtr = 4;
+//        items.add(new ToDo("read book"));
+//        items.get(items.size() - 1).markAsDone();
+//        items.add(new Deadline("return book", "June 6th"));
+//        items.add(new Event("project meeting", "Aug 6th 2pm", "4pm"));
+//        items.add(new ToDo("joins sports club"));
+//        items.get(items.size() - 1).markAsDone();
+//        items.add(new ToDo("borrow book"));
 //    }
+
+    private void printNoOfItems(ArrayList<Task> items) {
+        System.out.println("\nNow you have " + items.size() + " tasks stored in the list.");
+    }
 
     private void printHoriLine() {
         System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -49,16 +52,24 @@ public class NukNagnel {
                     printItemsList();
                     break;
                 case "mark":
-                    item = items[userInputScanner.nextInt()-1];
+                    item = items.get(userInputScanner.nextInt()-1);
                     item.markAsDone();
                     System.out.println("Awesome! The task below has been marked as *done*:");
                     System.out.print(item);
                     break;
                 case "unmark":
-                    item = items[userInputScanner.nextInt()-1];
+                    item = items.get(userInputScanner.nextInt()-1);
                     item.markAsUndone();
                     System.out.println("Alright, I have marked this task as *not done yet*");
                     System.out.print(item);
+                    break;
+                case "delete":
+                    int id = userInputScanner.nextInt()-1;
+                    item = items.get(id);
+                    items.remove(id);
+                    System.out.println("The task has been successfully removed.");
+                    System.out.print(item);
+                    printNoOfItems(items);
                     break;
                 default:
                     storeItem(command, userInputScanner.nextLine());
@@ -69,8 +80,8 @@ public class NukNagnel {
 
     private void printItemsList() {
         System.out.println("Below are the tasks stored in your list:");
-        for (int i = 0; i < items.length; i++) {
-            Task item = items[i];
+        for (int i = 0; i < items.size(); i++) {
+            Task item = items.get(i);
             if (item == null) break;
             String s = String.valueOf(i+1) + ". " + item;
             System.out.println(s);
@@ -79,13 +90,14 @@ public class NukNagnel {
 
     private void storeItem(String type, String item) {
         String[] cmd;
+        Task task = new Task("");
         switch (type) {
             case "todo":
-                items[itemsPtr] = new ToDo(item);
+                task = new ToDo(item);
                 break;
             case "deadline":
                 cmd = item.split("/by ");
-                items[itemsPtr] = new Deadline(cmd[0].stripTrailing(), cmd[1]);
+                task = new Deadline(cmd[0].stripTrailing(), cmd[1]);
                 break;
             case "event":
                 cmd = item.split("/from ");
@@ -93,11 +105,12 @@ public class NukNagnel {
                 cmd = cmd[1].split(" /to ");
                 String from = cmd[0];
                 String to = cmd[1];
-                items[itemsPtr] = new Event(desc, from, to);
+                task = new Event(desc, from, to);
                 break;
         }
-        System.out.println("I have added the task as requested:\n" + items[itemsPtr]);
-        System.out.println("Now you have " + String.valueOf(++itemsPtr) + " tasks stored in the list.");
+        items.add(task);
+        System.out.println("I have added the task as requested:\n" + task);
+        printNoOfItems(items);
     }
 
     public static void main(String[] args) {
