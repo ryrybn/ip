cd8e339b530db38f0d695c5d2d944e3f042d02ed
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-01-30 01:35:07 +0800

    NukNagnel.java: Added ability to store text entered by user, and displaying store contents.
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 public class NukNagnel {
-    private static void printHoriLine() {
+    private String[] items = new String[100];
+    private int itemsPtr = 0;
+
+    private void printHoriLine() {
         System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
-    private static void printIntro() {
+    private void printIntro() {
         printHoriLine();
         String intro = " Good day friend! My name is NukNagnel.\n" +
                 " What can I do you for?";
@@ -13,27 +16,47 @@ public class NukNagnel {
         printHoriLine();
     }
 
-    private static void printOutro() {
+    private void printOutro() {
         String exit = " This conversation has ended.\n Hope to chat again soon and have a splendid day ahead!";
         System.out.print(exit);
         printHoriLine();
     }
 
-    private static void echoComd() {
+    private void echoComd() {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         do {
             System.out.print(" > ");
-            userInput = scanner.next();
+            userInput = scanner.nextLine();
             printHoriLine();
-            System.out.print(userInput);
+            if (userInput.equals("list")) {
+                printItemsList();
+            } else {
+                storeItem(userInput);
+            }
             printHoriLine();
         } while (!userInput.equals("bye"));
     }
 
+    private void printItemsList() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) break;
+            System.out.print(i+1);
+            System.out.print(". ");
+            System.out.println(items[i]);
+        }
+    }
+
+    private void storeItem(String item) {
+        items[itemsPtr] = item;
+        itemsPtr++;
+        System.out.println("added: " + item);
+    }
+
     public static void main(String[] args) {
-        printIntro();
-        echoComd();
-        printOutro();
+        NukNagnel chat = new NukNagnel();
+        chat.printIntro();
+        chat.echoComd();
+        chat.printOutro();
     }
 }
