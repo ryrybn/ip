30ec7f5c321d3e8de6a71b6733069c866ee15223
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-01-30 02:34:10 +0800

    NukNagnel.java: Added ability to mark tasks as done and not done
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,8 +1,17 @@
 import java.util.*;
 
 public class NukNagnel {
-    private String[] items = new String[100];
+    private Task[] items = new Task[100];
     private int itemsPtr = 0;
+
+    // Sample data to test functionality
+//    private void samplePopulate() {
+//        items[0] = new Task("read book");
+//        items[0].markAsDone();
+//        items[1] = new Task("return book");
+//        items[2] = new Task("buy bread");
+//        itemsPtr = 3;
+//    }
 
     private void printHoriLine() {
         System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -22,41 +31,59 @@ public class NukNagnel {
         printHoriLine();
     }
 
-    private void echoComd() {
+    private void taskTracker() {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         do {
             System.out.print(" > ");
             userInput = scanner.nextLine();
             printHoriLine();
-            if (userInput.equals("list")) {
-                printItemsList();
-            } else {
-                storeItem(userInput);
+            Scanner userInputScanner = new Scanner(userInput);
+            Task item;
+            switch(userInputScanner.next()) {
+                case "list":
+                    printItemsList();
+                    break;
+                case "mark":
+                    item = items[userInputScanner.nextInt()-1];
+                    item.markAsDone();
+                    System.out.println("Awesome! The task below has been marked as *done*:");
+                    System.out.print(item);
+                    break;
+                case "unmark":
+                    item = items[userInputScanner.nextInt()-1];
+                    item.markAsUndone();
+                    System.out.println("Alright, I have marked this task as *not done yet*");
+                    System.out.print(item);
+                    break;
+                default:
+                    storeItem(userInput);
             }
             printHoriLine();
         } while (!userInput.equals("bye"));
     }
 
     private void printItemsList() {
+        System.out.println("Below are the tasks stored in your list:");
         for (int i = 0; i < items.length; i++) {
-            if (items[i] == null) break;
-            System.out.print(i+1);
-            System.out.print(". ");
-            System.out.println(items[i]);
+            Task item = items[i];
+            if (item == null) break;
+            String s = String.valueOf(i+1) + ". " + item;
+            System.out.println(s);
         }
     }
 
     private void storeItem(String item) {
-        items[itemsPtr] = item;
+        items[itemsPtr] = new Task(item);
         itemsPtr++;
         System.out.println("added: " + item);
     }
 
     public static void main(String[] args) {
         NukNagnel chat = new NukNagnel();
+//        chat.samplePopulate();
         chat.printIntro();
-        chat.echoComd();
+        chat.taskTracker();
         chat.printOutro();
     }
 }
