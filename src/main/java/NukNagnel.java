8231be127cc6e1f5b1ba47a00382b4abf4bea30f
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-01-30 01:06:05 +0800

    NukNagnel.java: Implemented echo command function
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>

--- src/main/java/NukNagnel.java
+++ src/main/java/NukNagnel.java
@@ -1,24 +1,39 @@
+import java.util.*;
+
 public class NukNagnel {
     private static void printHoriLine() {
-        System.out.println("\n____________________________________________________________");
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
     private static void printIntro() {
         printHoriLine();
-        String intro = " Hello! I'm NukNagnel\n" +
-                " What can I do for you?";
+        String intro = " Good day friend! My name is NukNagnel.\n" +
+                " What can I do you for?";
         System.out.print(intro);
         printHoriLine();
     }
 
     private static void printOutro() {
-        String exit = " Bye. Hope to see you again soon!";
+        String exit = " This conversation has ended.\n Hope to chat again soon and have a splendid day ahead!";
         System.out.print(exit);
         printHoriLine();
     }
 
+    private static void echoComd() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        do {
+            System.out.print(" > ");
+            userInput = scanner.next();
+            printHoriLine();
+            System.out.print(userInput);
+            printHoriLine();
+        } while (!userInput.equals("bye"));
+    }
+
     public static void main(String[] args) {
         printIntro();
+        echoComd();
         printOutro();
     }
 }
