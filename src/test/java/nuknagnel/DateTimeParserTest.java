fc3f1e917dd8f94900a5072958aac11b3b352e77
Neo Ryan <63915587+ryrybn@users.noreply.github.com>
2026-02-06 10:56:44 +0800

    Add JUnit tests for command type detection as well as DateTime valid input
    Signed-off-by: Neo Ryan <63915587+ryrybn@users.noreply.github.com>

---
+++ src/test/java/nuknagnel/DateTimeParserTest.java
@@ -0,0 +1,57 @@
+package nuknagnel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class DateTimeParserTest {
+
+    @Test
+    public void parseDate_validIsoDate_success() {
+        LocalDate date = DateTimeParser.parseDate("2024-02-01");
+
+        assertEquals(LocalDate.of(2024, 2, 1), date);
+    }
+
+    @Test
+    public void parseDate_invalidFormat_throws() {
+        InvalidInputException exception = assertThrows(InvalidInputException.class,
+                () -> DateTimeParser.parseDate("02-01-2024"));
+
+        assertEquals("Please use yyyy-mm-dd for dates.", exception.getMessage());
+    }
+
+    @Test
+    public void parseDateTime_isoFormat_success() {
+        LocalDateTime dateTime = DateTimeParser.parseDateTime("2024-02-01T13:45");
+
+        assertEquals(LocalDateTime.of(2024, 2, 1, 13, 45), dateTime);
+    }
+
+    @Test
+    public void parseDateTime_compactFormat_success() {
+        LocalDateTime dateTime = DateTimeParser.parseDateTime("2024-02-01 1345");
+
+        assertEquals(LocalDateTime.of(2024, 2, 1, 13, 45), dateTime);
+    }
+
+    @Test
+    public void parseDateTime_spacedFormat_success() {
+        LocalDateTime dateTime = DateTimeParser.parseDateTime("2024-02-01 13:45");
+
+        assertEquals(LocalDateTime.of(2024, 2, 1, 13, 45), dateTime);
+    }
+
+    @Test
+    public void parseDateTime_invalidFormat_throws() {
+        InvalidInputException exception = assertThrows(InvalidInputException.class,
+                () -> DateTimeParser.parseDateTime("2024/02/01 13:45"));
+
+        assertEquals("Please use yyyy-mm-dd HHmm or yyyy-mm-dd HH:mm for date-time.",
+                exception.getMessage());
+    }
+}
---
+++ src/test/java/nuknagnel/ParserTest.java
@@ -0,0 +1,74 @@
+package nuknagnel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void parse_listCommand_success() {
+        ParsedCommand command = Parser.parse("list");
+
+        assertEquals(ParsedCommand.Type.LIST, command.getType());
+    }
+
+    @Test
+    public void parse_markCommand_success() {
+        ParsedCommand command = Parser.parse("mark 2");
+
+        assertEquals(ParsedCommand.Type.MARK, command.getType());
+        assertEquals(1, command.getIndex());
+    }
+
+    @Test
+    public void parse_todoCommand_success() {
+        ParsedCommand command = Parser.parse("todo read book");
+
+        assertEquals(ParsedCommand.Type.TODO, command.getType());
+        assertInstanceOf(ToDo.class, command.getTask());
+        assertEquals("[T][ ] read book", command.getTask().toString());
+    }
+
+    @Test
+    public void parse_deadlineCommand_success() {
+        ParsedCommand command = Parser.parse("deadline return book /by 2024-02-03");
+
+        assertEquals(ParsedCommand.Type.DEADLINE, command.getType());
+        assertInstanceOf(Deadline.class, command.getTask());
+        assertEquals("[D][ ] return book (by: Feb 3 2024)", command.getTask().toString());
+    }
+
+    @Test
+    public void parse_eventCommand_success() {
+        ParsedCommand command = Parser.parse("event meeting /from 2024-02-03 1200 /to 2024-02-03 1300");
+
+        assertEquals(ParsedCommand.Type.EVENT, command.getType());
+        assertInstanceOf(Event.class, command.getTask());
+        assertEquals("[E][ ] meeting (from: Feb 3 2024 12:00 to: Feb 3 2024 13:00)",
+                command.getTask().toString());
+    }
+
+    @Test
+    public void parse_missingIndex_throws() {
+        InvalidInputException exception = assertThrows(InvalidInputException.class, () -> Parser.parse("mark"));
+
+        assertEquals("Please input the required parameters for your command.", exception.getMessage());
+    }
+
+    @Test
+    public void parse_invalidIndex_throws() {
+        InvalidInputException exception = assertThrows(InvalidInputException.class, () -> Parser.parse("delete 0"));
+
+        assertEquals("Please provide a valid task number.", exception.getMessage());
+    }
+
+    @Test
+    public void parse_missingTodoDescription_throws() {
+        InvalidInputException exception = assertThrows(InvalidInputException.class, () -> Parser.parse("todo "));
+
+        assertEquals("Todo tasks must include a description.", exception.getMessage());
+    }
+}
